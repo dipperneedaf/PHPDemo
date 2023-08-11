6921030dabf92a3c976ab50b96fd5b743b8a7738
Duong Phong <131778675+dipperneedaf@users.noreply.github.com>
2023-08-11 08:30:03 +0700

    Update uploadform.php

--- uploadform.php
+++ uploadform.php
@@ -29,9 +29,31 @@ p{
 <body>
 
 <div>
-<input type="text" placeholder="Tên đăng nhập, số điện thoại, gmail" id="fname" name="fname"><br><br>
-<input type="text" placeholder="Mật khẩu" id="password" name="password"><br><br>
-<button id = "button1" type="button">Đăng nhập</button><br>
+<form action="reg.php" method="post" enctype="multipart/form-data">
+Avatar*
+<input type="file" name="fileupload" id="fileupload"><br><br>
+
+
+
+
+
+
+<label for="fname">Name*</label>
+<input id = "input1" type="text" id="name" name="name"><br><br>
+<label for="fname">Email*</label>
+<input id = "input1" type="text" id="email" name="email"><br><br>
+<label for="lname">Password*</label>
+<input id = "input1" type="text" id="password" name="password"><br><br>
+<label for="lname">Password verify*</label>
+<input id = "input1" type="text" id="passwordv" name="passwordv"><br><br>
+<p>Roles* <br>
+<input type="checkbox" id="role1" name="role" value="Superadmin">
+  <label for="role1"> Super admin</label><br>
+  <input type="checkbox" id="role2" name="role" value="Admin">
+  <label for="role2"> Admin</label><br>
+</p>
+<input id ="button1" type="submit" value="Save">
+</form>
 </div>
 </body>
 </html>
